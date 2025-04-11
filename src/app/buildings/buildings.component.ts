72f0a3c670c22188478603e8b338c0e631386ed2
tk <tk@example.org>
2025-04-11 16:38:16 +0200

    Add components and nav.

---
+++ src/app/buildings/buildings.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buildings',
+  imports: [],
+  templateUrl: './buildings.component.html',
+  styleUrl: './buildings.component.css'
+})
+export class BuildingsComponent {
+
+}
---
+++ src/app/research/research.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-research',
+  imports: [],
+  templateUrl: './research.component.html',
+  styleUrl: './research.component.css'
+})
+export class ResearchComponent {
+
+}
